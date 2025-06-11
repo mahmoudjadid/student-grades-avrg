38eee642d81ced4cb4d389c01e390c1ead40b96e
mahmoudjadid <mahmoudjadid@outlook.com>
2025-06-11 22:25:15 +0300

    added inputs

--- student-grades-avrg.py
+++ student-grades-avrg.py
@@ -1,6 +1,8 @@
 
 # im having issues on VS code , cant push normally
-
+st_names=[]
+st_grade=[]
+    
 def display_student_summary(names, grades):
     for name , grade in (names, grades):
         print(f"{name}, {grade}")
@@ -25,20 +27,18 @@ def count_passed(names, grades):
             print ("number of passes",count) 
 
 def main ():
-    students_nmber=int(input("enter number of students : "))
-    st_names=[]
-    st_grade=[]
+    students_nmber=int(input("enter number of students : "))    
     for i in range (students_nmber):
         name = input(str("enter name :"))
         grade = float(input("enter grade :"))
         st_grade.append(grade)
         st_names.append(name)
         
-
-display_student_summary(["omar", "mahmoud"], [1,2])
-# get_avg_grade(grades)
+main ()
+display_student_summary(st_names,st_grade)
+# get_avg_grade([])
 # get_heighest_grade(names, grades)
 # count_passed(names, grades)
-# main ()
+
 
 
